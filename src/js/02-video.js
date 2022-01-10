b40620700068280aaaec2afdddd8668551fea4d5
Viacheslav Aleksandrov <rimondtt@gmail.com>
2022-01-11 00:33:15 +0200

    task-02-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+loadTime();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+}
+
+function loadTime() {
+    if (localStorage.getItem("videoplayer-current-time")){
+        player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    }
+}
