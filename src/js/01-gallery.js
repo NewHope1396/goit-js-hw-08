e43058cf9f9c9173ef930d2828c383130e4468cd
Viacheslav Aleksandrov <rimondtt@gmail.com>
2022-01-10 23:22:30 +0200

    task-01-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const containerRef = document.querySelector('.gallery');
+
+const imagesHtml = createImagesHtml(galleryItems);
+
+containerRef.insertAdjacentHTML('beforeend' ,imagesHtml);
+
+
+function createImagesHtml(images) {
+    return galleryItems.map((img) => {
+        return `<a class="gallery__item" href="${img.original}">
+                        <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+                </a>`
+    }).join('')
+}
+
+const gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+containerRef.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+
+    gallery.next()
+}
