6d81ee80d47ab217ebd817acdd86a330e57c5ec6
Viacheslav Aleksandrov <rimondtt@gmail.com>
2022-01-11 02:32:00 +0200

    task-03-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+const form = document.querySelector('form');
+const mailInput = document.querySelector('[name = "email"]');
+const messageInput = document.querySelector('[name = "message"]');
+
+
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+const data = {};
+
+onReload();
+
+function onInput(evt) {
+    data[evt.target.name] = evt.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data))
+}
+
+function onReload() {
+    try {
+        const loadedData = JSON.parse(localStorage.getItem("feedback-form-state"))
+        if (loadedData.email) {
+            mailInput.value = loadedData.email;
+        }
+        
+        if (loadedData.message) {
+            messageInput.value = loadedData.message;
+        }
+
+        Object.assign(data, loadedData);
+    } catch (error) {
+        console.log('Надо что-то написать сначала')
+    }
+
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    localStorage.removeItem("feedback-form-state");
+    console.log(data);
+
+    evt.target.reset();
+}
+
 
